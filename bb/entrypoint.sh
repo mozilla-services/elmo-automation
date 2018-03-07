09dfbd6ed81a8a1401e877f411db43c999c4a58a
Axel Hecht <axel@pike.org>
2018-03-07 11:47:06 +0100

    Create buildbot master and slave as part of docker build process

--- bb/entrypoint.sh
+++ bb/entrypoint.sh
@@ -3,7 +3,7 @@
 cleanup_bots() {
     echo "should shut down buildbot"
     ./master-ball/scripts/buildbot stop master-ball/slave
-    ./master-ball/scripts/buildbot stop master-ball/test-master
+    ./master-ball/scripts/buildbot stop master-ball/l10n-master
     echo "should have shut down buildbot"
     exit 0
 }
@@ -16,7 +16,7 @@ trap "cleanup_bots" SIGTERM
 cd /app/
 
 # start master and slave
-./master-ball/scripts/buildbot start master-ball/test-master
+./master-ball/scripts/buildbot start master-ball/l10n-master
 ./master-ball/scripts/buildbot start master-ball/slave
 
 echo "[hit enter key to exit] or run 'docker stop <container>'"
